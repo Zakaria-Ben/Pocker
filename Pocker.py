cdc4add3b1a268a7b218253237bc1ad0c1721ad6
Zakaria <zakaria.benomar06@gmail.com>
2019-04-08 16:34:17 +0200

    first commit

---
+++ Pocker.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python2.7
+
+import sys
+import subprocess
+import os
+import ctypes
+from unshare import unshare
+from unshare import setns
+from sh import mount
+
+def run():
+    i = 2
+    string_cmd = ""
+    while i<len(sys.argv):
+        string_cmd = string_cmd + str(sys.argv[i]) + " "
+        i +=1
+
+    print("running " + string_cmd )
+    os.chroot("/ubuntu_xenial_1604")
+    os.chdir("/")
+    os.system("mount -t proc proc /proc")
+    unshare(131072)
+    unshare(1073741824)
+    unshare(67108864)
+    os.system(str(string_cmd))
+
+if __name__ == '__main__':
+    if sys.argv[1] == "run":
+        run()
+    else :
+        print("Whaaaat!!")
